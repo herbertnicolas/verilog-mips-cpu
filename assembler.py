5c6b4da410a2ad07efc0490f5a4230f351180123
Gustavo Farani <gff@cin.ufpe.br>
2022-08-29 20:13:23 -0300

    parser has all instructions

--- assembler.py
+++ assembler.py
@@ -1,5 +1,6 @@
 import pyparsing as pp
 import json
+from bitstring import BitArray
 
 register = '$' + (pp.oneOf('zero at gp sp fp ra') | pp.Regex(r'v[0-1]') | pp.Regex(r'a[0-3]') | pp.Regex(r't[0-9]') | pp.Regex(r's[0-7]') | pp.Regex(r'k[0-1]'))
 
@@ -36,121 +37,106 @@ with open('funct.json') as file:
     global funct_dict
     funct_dict = json.load(file)
 
+def r_format(rs, rt, rd, shamt, funct):
+    opcode = BitArray(uint=0, length=6)
+    rs = BitArray(uint=rs, length=5)
+    rt = BitArray(uint=rt, length=5)
+    rd = BitArray(uint=rd, length=5)
+    shamt = BitArray(uint=shamt, length=5)
+    funct = BitArray(uint=int(funct_dict[funct], 0), length=6)
+    return opcode + rs + rt + rd + shamt + funct
+
+instruction_name_r_rd_rs_rt = pp.oneOf('add and sllv slt srav sub')
+instruction_r_rd_rs_rt = instruction_name_r_rd_rs_rt + register + pp.Char(',') + register + pp.Char(',') + register
+instruction_r_rd_rs_rt.setParseAction(lambda tk: r_format(tk[3], tk[5], tk[1], 0, tk[0]) )
+
+instruction_name_r_div_mult = pp.oneOf('div mult')
+instruction_r_div_mult = instruction_name_r_div_mult + register + pp.Char(',') + register
+instruction_r_div_mult.setParseAction(lambda tk: r_format(tk[1], tk[3], 0, 0, tk[0]) )
+
+instruction_name_hi_lo = pp.oneOf('mfhi mflo')
+instruction_r_hi_lo = instruction_name_hi_lo + register
+instruction_r_hi_lo.setParseAction(lambda tk: r_format(0, 0, tk[1], 0, tk[0]) )
+
+instruction_name_r_jr = pp.Literal('jr')
+instruction_r_jr = instruction_name_r_jr + register
+instruction_r_jr.setParseAction(lambda tk: r_format(tk[1], 0, 0, 0, tk[0]) )
+
+instruction_name_r_shift = pp.oneOf('sll sra srl')
+instruction_r_shift = instruction_name_r_shift + register + pp.Char(',') + register + pp.Char(',') + pp.pyparsing_common.integer
+instruction_r_shift.setParseAction(lambda tk: r_format(0, tk[3], tk[1], tk[5], tk[0]) )
+
+instruction_r_op = pp.oneOf('break rte')
+instruction_r_op.setParseAction(lambda tk: r_format(0, 0, 0, 0, tk[0]) )
+
+instruction_name_r_sp = pp.oneOf('push pop')
+instruction_r_sp = instruction_name_r_sp + register
+instruction_r_sp.setParseAction(lambda tk: r_format(0, tk[1], 0, 0, tk[0]))
+
+instruction_r = instruction_r_rd_rs_rt | instruction_r_div_mult | instruction_r_hi_lo | instruction_r_jr | instruction_r_shift | instruction_r_op | instruction_r_sp
+
 with open('opcode.json') as file:
     global opcode_dict
     opcode_dict = json.load(file)
 
-# def funct(r_format):
-#     match r_format:
-#         case 'add':
-#             return int('0x20', 0)
-#         case 'and':
-#             return int('0x24', 0)
-#         case 'div':
-#             return int('0x1a', 0)
-#         case 'mult':
-#             return int('0x18', 0)
-#         case 'jr':
-#             return int('0x8', 0)
-#         case 'mfhi':
-#             return int('0x10', 0)
-#         case 'mflo':
-#             return int('0x12', 0)
-#         case 'sll':
-#             return int('0x0', 0)
-#         case 'sllv':
-#             return int('0x4', 0)
-#         case 'slt':
-#             return int('0x2a', 0)
-#         case 'sra':
-#             return int('0x3', 0)
-#         case 'srav':
-#             return int('0x7', 0)
-#         case 'srl':
-#             return int('0x2', 0)
-#         case 'sub':
-#             return int('0x22', 0)
-#         case 'break':
-#             return int('0xd', 0)
-#         case 'rte':
-#             return int('0x13', 0)
-#         case 'addm':
-#             return int('0x5', 0)
+def i_format(opcode, rs, rt, offset_immediate):
+    opcode = BitArray(uint=int(opcode_dict[opcode], 0), length=6)
+    rs = BitArray(uint=rs, length=5)
+    rt = BitArray(uint=rt, length=5)
+    offset_immediate = BitArray(int=offset_immediate, length=16)
+    return opcode + rs + rt + offset_immediate
 
-#register.setParseAction(pp.tokenMap(parseRegister))
-#print(register.matches('$t0'))
-#print(pp.pyparsing_common.integer.parseString('1000000000000000000000000007000000000000'))
-#pp.pyparsing_common.signed_integer
-def r_format(rs, rt, rd, shamt, funct):
-    if shamt >= 32:
-        raise Exception('shamt out of bounds')
-    return (rs << 21) ^ (rt << 16) ^ (rd << 11) ^ (shamt << 6) ^ funct
+instruction_name_i_immediate = pp.oneOf('addi addiu slti')
+instruction_i_immediate = instruction_name_i_immediate + register + pp.Char(',') + register + pp.Char(',') + pp.pyparsing_common.signed_integer
+instruction_i_immediate.setParseAction(lambda tk: i_format(tk[0], tk[3], tk[1], tk[5]))
 
-instruction_name_r_rd_rs_rt = pp.oneOf('add and sllv slt srav sub addm')
-# = pp.Literal('add') | pp.Literal('and') | pp.Literal('sllv') | pp.Literal('slt') | pp.Literal('srav') | pp.Literal('sub') | pp.Literal('addm')
-instruction_r_rd_rs_rt = instruction_name_r_rd_rs_rt + register + pp.Char(',') + register + pp.Char(',') + register
-instruction_r_rd_rs_rt.setParseAction(lambda tk: r_format(tk[3], tk[5], tk[1], 0, int(funct_dict[tk[0]], 0) ))
+instruction_name_i_branch = pp.oneOf('beq bne ble bgt')
+instruction_i_branch = instruction_name_i_branch + register + pp.Char(',') + register + pp.Char(',') + pp.pyparsing_common.signed_integer
+instruction_i_branch.setParseAction(lambda tk: i_format(tk[0], tk[1], tk[3], tk[5]))
 
-instruction_name_r_rs_rt = pp.oneOf('div mult')
-# pp.Literal('div') | pp.Literal('mult')
-instruction_r_rs_rt = instruction_name_r_rs_rt + register + pp.Char(',') + register
-instruction_r_rs_rt.setParseAction(lambda tk: r_format(tk[1], tk[3], 0, 0, int(funct_dict[tk[0]], 0) ))
+instruction_name_i_memory = pp.oneOf('lb lh lw sb sh sw')
+instruction_i_memory = instruction_name_i_memory + register + pp.Char(',') + pp.pyparsing_common.signed_integer + pp.Char('(') + register + pp.Char(')')
+instruction_i_memory.setParseAction(lambda tk: i_format(tk[0], tk[5], tk[1], tk[3]))
 
-instruction_name_r_rs = pp.Literal('jr')
-instruction_r_rs = instruction_name_r_rs + register
-instruction_r_rs.setParseAction(lambda tk: r_format(tk[1], 0, 0, 0, int(funct_dict[tk[0]], 0) ))
+instruction_name_i_lui = pp.Literal('lui')
+instruction_i_lui = instruction_name_i_lui + register + pp.Char(',') + pp.pyparsing_common.signed_integer
+instruction_i_lui.setParseAction(lambda tk: i_format(tk[0], 0, tk[1], tk[3]))
 
-instruction_name_r_rd = pp.oneOf('mfhi mflo')
-# pp.Literal('mfhi') | pp.Literal('mflo')
-instruction_r_rd = instruction_name_r_rd + register
-instruction_r_rd.setParseAction(lambda tk: r_format(0, 0, tk[1], 0, int(funct_dict[tk[0]], 0) ))
+instruction_i = instruction_i_immediate | instruction_i_branch | instruction_i_memory | instruction_i_lui
 
-instruction_name_r_rd_rt_shamt = pp.oneOf('sll sra srl')
-# pp.Literal('sll') | pp.Literal('sra') | pp.Literal('srl')
-instruction_r_rd_rt_shamt = instruction_name_r_rd_rt_shamt + register + pp.Char(',') + register + pp.Char(',') + pp.pyparsing_common.integer
-instruction_r_rd_rt_shamt.setParseAction(lambda tk: r_format(0, tk[3], tk[1], tk[5], int(funct_dict[tk[0]], 0) ))
+def j_format(opcode, offset):
+    opcode = BitArray(uint=int(opcode_dict[opcode], 0), length=6)
+    offset = BitArray(int=offset, length=26)
+    return opcode + offset
 
-instruction_r_ = pp.oneOf('break rte invalid')
-# pp.Literal('break') | pp.Literal('rte')
-instruction_r_.setParseAction(lambda tk: r_format(0, 0, 0, 0, int(funct_dict[tk[0]], 0) ))
+instruction_name_j = pp.oneOf('j jal')
+instruction_j = instruction_name_j + pp.pyparsing_common.signed_integer
+instruction_j.setParseAction(lambda tk: j_format(tk[0], tk[1]))
 
-cap = lambda n: (1 << n) - 1
+instruction = instruction_r | instruction_i | instruction_j
 
-def r_format_to_assembly(bitmask):
-    return {
-        'funct': hex(bitmask & cap(6)),
-        'shamt': (bitmask & cap(11)) >> 6,
-        'rd': (bitmask & cap(16)) >> 11,
-        'rt': (bitmask & cap(21)) >> 16,
-        'rs': (bitmask & cap(26)) >> 21
-    }
+mem = ['00']*256
 
-# bitmask = instruction_r_rd_rs_rt.parse_file('instructions.asm')
-# print(f'{bitmask[0]:032b}')
-# print(f'{bitmask[0]:08X}')
-# print(r_format_to_assembly(bitmask[0]))
-# print(register.parse_string('$s0'))
-instruction_r = instruction_r_rd_rs_rt | instruction_r_rs_rt | instruction_r_rs | instruction_r_rd | instruction_r_rd_rt_shamt | instruction_r_
-mem = [0]*256
+for address, byte in zip(range(228, 232), instruction_r_op.parse_string('rte')[0].cut(8)):
+    mem[address] = byte.hex
 
 # o endereço das rotinas de tratamento das exceções
 for i in range(253, 256):
-    mem[i] = i - 3
+    mem[i] = BitArray(uint=228, length=8).hex
 
 with open('instructions.asm') as file:
     address = 0
     for line in file.readlines():
-        bitmask = instruction_r.parse_string(line)[0]
-        for i in range(4):
-            mem[address + 3 - i] = (bitmask >> (i << 3)) & 255;
-        address += 4
+        word = instruction.parse_string(line)[0]
+        for byte in word.cut(8):
+            mem[address] = byte.hex
+            address += 1
 
 with open('mem_init_data.hex', 'w') as file:
     for i in range(0, 256, 4):
         for j in range(i, i + 4):
             # format string 02x quer dizer hexadecimal
             # e padding de 2 zeros à esquerda
-            file.write(f'{mem[j]:02X} ')
+            file.write(mem[j] + ' ')
         # n coloca o newline automaticamente igual ao print
         file.write('\n')
-# print(r_format_to_assembly(instruction_r_rd_rs_rt.parse_string('add $t2, $t1, $t0')[0]) )
--- instructions.asm
+++ instructions.asm
@@ -1,1 +1,5 @@
-invalid
+lui $t0, 32767
+addi $t0, $t0, 32767
+lui $t1, 32767
+addi $t1, $t1, 1
+add $t2, $t0, $t1
